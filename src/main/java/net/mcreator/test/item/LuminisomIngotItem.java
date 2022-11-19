09df6c62a156c9467ece01dba44b0b924f816d2b
pokeyt59 <alexsuper199@mail.ru>
2022-11-19 20:20:18 +0300

    Thornreethelmet update

--- src/main/java/net/mcreator/test/item/LuminisomIngotItem.java
+++ src/main/java/net/mcreator/test/item/LuminisomIngotItem.java
@@ -1,13 +1,12 @@
 
 package net.mcreator.test.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import javax.annotation.Nullable;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
 
 public class LuminisomIngotItem extends Item {
-
 	public LuminisomIngotItem() {
 		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
 	}
-
 }
--- src/main/java/net/mcreator/test/procedures/GekolibProcedure.java
+++ src/main/java/net/mcreator/test/procedures/GekolibProcedure.java
@@ -1,9 +1,5 @@
 package net.mcreator.test.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
-
-import javax.annotation.Nullable;
-
 public class GekolibProcedure {
 	public static void execute() {
 	}
